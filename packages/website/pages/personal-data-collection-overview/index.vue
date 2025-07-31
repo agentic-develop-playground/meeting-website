4b897453c06c5852aecc5b399131ec3926f1e310
wuming230 <1819845645@qq.com>
2025-07-31 10:42:17 +0800

    feat: 个人信息收集清单

---
+++ packages/website/pages/personal-data-collection-overview/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import ThePersonalDataCollectionOverview from '@/views/personal-data-collection-overview/ThePersonalDataCollectionOverview.vue';
+</script>
+
+<template>
+  <ThePersonalDataCollectionOverview />
+</template>
+
+<style lang="scss" scoped></style>
---
+++ packages/website/views/personal-data-collection-overview/ThePersonalDataCollectionOverview.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import Markdown from 'markdown-it';
+import personalDataCollectionOverview from '@/config/markdown/personal-data-collection-overview.md?raw';
+
+const md = new Markdown({
+  html: true,
+  linkify: true,
+});
+</script>
+
+<template>
+  <ContentWrapper>
+    <div v-dompurify-html="md.render(personalDataCollectionOverview)" class="markdown legal-markdown"></div>
+  </ContentWrapper>
+</template>
+
+<style lang="scss" scoped>
+.content-wrapper {
+  padding-top: 72px;
+  padding-bottom: 72px;
+  @include respond-to('phone') {
+    padding-top: 32px;
+    padding-bottom: 32px;
+  }
+}
+:deep(.legal-markdown) {
+  max-width: 880px;
+  margin: 0 auto;
+  h1 {
+    text-align: center;
+    padding-bottom: 40px;
+    border-bottom: 1px solid var(--o-color-control4);
+    @include display3;
+    font-weight: 500;
+    margin-bottom: 40px;
+    @include respond-to('phone') {
+      text-align: left;
+      padding-bottom: 16px;
+      margin-bottom: 24px;
+      @include display1;
+    }
+  }
+  h2 {
+    margin-bottom: 16px;
+    font-weight: 500;
+    @include respond-to('phone') {
+      margin: 24px 0 12px;
+    }
+  }
+}
+</style>
