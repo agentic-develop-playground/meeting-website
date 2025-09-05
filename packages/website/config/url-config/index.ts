08d869cd1c55661d46d79aa1ee91281060605b60
wuming230 <1819845645@qq.com>
2025-09-05 11:35:33 +0800

    fix: 修改链接

--- packages/website/config/url-config/index.ts
+++ packages/website/config/url-config/index.ts
@@ -6,6 +6,6 @@ const HIKUNPENG_URL = 'https://www.hikunpeng.com/';
 const HUAWEICLOUD_URL = 'https://www.huaweicloud.com/';
 const OPENI_URL = 'https://www.openi.org.cn/';
 
-const HWCLOUDTEST = 'https://lfupmirror1.hwcloudtest.cn:18443';
+const HWCLOUDTEST = 'https://id1.cloud.huawei.com';
 
 export { HIASCEND_URL, MINDSPORE_URL, HUAWEI_URL, EHUAWEI_URL, HIKUNPENG_URL, HUAWEICLOUD_URL, OPENI_URL, HWCLOUDTEST };
